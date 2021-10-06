3ad015d856845a16b17af0ce77bd12621f4d7f98
Jeremyguiller <guillerjeremy@gmail.com>
2021-10-06 10:20:11 +0200

    ajout .h

---
+++ Tri_insertion.h
@@ -0,0 +1,8 @@
+//
+// Created by j.guillerminprina on 06/10/2021.
+//
+
+#ifndef OBJETTP2_TRI_INSERTION_H
+#define OBJETTP2_TRI_INSERTION_H
+
+#endif //OBJETTP2_TRI_INSERTION_H
---
+++ Tri_selection.h
@@ -0,0 +1,8 @@
+//
+// Created by j.guillerminprina on 06/10/2021.
+//
+
+#ifndef OBJETTP2_TRI_SELECTION_H
+#define OBJETTP2_TRI_SELECTION_H
+
+#endif //OBJETTP2_TRI_SELECTION_H
