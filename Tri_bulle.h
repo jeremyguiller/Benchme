293ff642a773cd0d2e13270af2e8d20caed2312b
BENUCCI <maxime.benucci@etu.univ-st-etienne.fr>
2021-10-06 17:27:23 +0200

    Tri bulle OK!

--- Tri_bulle.h
+++ Tri_bulle.h
@@ -1,8 +1,25 @@
-//
-// Created by j.guillerminprina on 06/10/2021.
-//
-
 #ifndef OBJETTP2_TRI_BULLE_H
 #define OBJETTP2_TRI_BULLE_H
+
+void Tri_bulle(float tab[]){
+    float tmp;
+    int i,j,size=5,changeA=0, changeB=0;
+    for(i=0; i<=size; i++) { //Boucle parcours le tableau
+        for (j=0; j<size-1; j++) { //Boucle pour comparer les couples
+            if (tab[j] > tab[j+1]) { //Changement de valeurs
+                tmp = tab[j];
+                tab[j] = tab[j+1];
+                tab[j+1] = tmp;
+                changeB++;
+            }
+        }
+
+        if (changeA==changeB){ //Si pas de changement, sortir de la boucle
+            break;
+        }
+        changeB=0;
+
+    }
+}
 
 #endif //OBJETTP2_TRI_BULLE_H
