fe5f38b6a617781d29b40c1e55a897a0173da5bf
Jeremyguiller <guillerjeremy@gmail.com>
2021-10-08 09:17:54 +0200

    version 1.0

--- main.c
+++ main.c
@@ -5,18 +5,5 @@
 #include "tri_tas.h"
 
 int main() {
-    int i;
-    float tab[5]={ 5.2,2.5,4.4,6.6,3.2 };
-    const int TAILLE = 5;
-    for (i=0;i<5;i++){
-        printf("%.2f ",tab[i]);
-    }
-    printf("\n");
-    Tri_tas(tab,TAILLE);
-    for (i=0;i<5;i++){
-        printf("%.2f ",tab[i]);
-    }
-
-
 
 }
